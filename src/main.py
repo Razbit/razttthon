81a56eda50acb20a2281bd9e2b6f2d2c6f679a7d
Razbit <razclocker@gmail.com>
2014-01-26 21:24:31 +0200

    Game functionality and shit

--- src/main.py
+++ src/main.py
@@ -16,10 +16,51 @@
 #
 ################################################################################
 
-#This file contains the main game loop
+#This file contains the main loop
 
-import player
+from mainc import cMain
+from player import cPlayerHandler
+from gameloop import cGame
 
-raz = player.Player(0)
-print raz.getName()
-print raz.getGames()
+def parsePlayerFile():
+    try:
+        playerfile = open("players.save", "r+")
+    except IOError:
+        print "No players.save file available."
+        cMain.playerlist = []
+        return
+
+    line = "a"
+    while line != "": #Read through the file
+        line = playerfile.readline()
+        
+        try:
+            if line[0] == '#': # comments
+                continue
+        except IndexError:
+            pass
+
+        line[:-1] #Remove leading newline
+        cMain.playerlist.append(line.split())
+    
+    try:
+        cMain.playerlist.pop() #Remove last item, it is garbage
+    except IndexError:
+        pass
+    
+    for index in range(len(cMain.playerlist)):
+        cMain.playerlist[index][1] = int(cMain.playerlist[index][1])
+        cMain.playerlist[index][2] = int(cMain.playerlist[index][2])
+        cMain.playerlist[index][3] = int(cMain.playerlist[index][3])
+        cMain.playerlist[index][4] = int(cMain.playerlist[index][4])
+        
+
+parsePlayerFile()
+phandler = cPlayerHandler()
+
+#Testing...
+game = cGame(1,2)
+
+print phandler.getData(1)
+print phandler.getData(2)
+print "Bye!"
