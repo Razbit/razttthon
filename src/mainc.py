c0370fe204d493f23bb40e9cabe07b394450d5c8
Razbit <razclocker@gmail.com>
2014-01-26 22:47:11 +0200

    Wrapper class cRazttthon created

--- src/mainc.py
+++ src/mainc.py
@@ -18,41 +18,76 @@
 
 #This file contains the main class and save io
 
-def parsePlayerFile():
-    try:
-        playerfile = open("players.save", "r+")
-    except IOError:
-        print "No players.save file available."
-        cMain.playerlist = []
-        return
-
-    line = "a"
-    while line != "": #Read through the file
-        line = playerfile.readline()
-        
-        try:
-            if line[0] == '#': # comments
-                continue
-        except IndexError:
-            pass
-
-        line[:-1] #Remove leading newline
-        cMain.playerlist.append(line.split())
-    
-    try:
-        cMain.playerlist.pop() #Remove last item, it is garbage
-    except IndexError:
-        pass
-    
-    for index in range(len(cMain.playerlist)):
-        cMain.playerlist[index][1] = int(cMain.playerlist[index][1])
-        cMain.playerlist[index][2] = int(cMain.playerlist[index][2])
-        cMain.playerlist[index][3] = int(cMain.playerlist[index][3])
-        cMain.playerlist[index][4] = int(cMain.playerlist[index][4])
-
 class cMain:
     
     playerlist=[]
 
     def __init__(self):
         pass
+
+from razlib import fprintf
+from player import cPlayerHandler
+from gameloop import cGame
+
+class cRazttthon(cMain, cPlayerHandler, cGame):
+
+    def parsePlayerFile(self):
+        try:
+            playerfile = open("players.save", "r")
+        except IOError:
+            print "No players.save file available."
+            cMain.playerlist = []
+            return
+
+        line = "a"
+        while line != "": #Read through the file
+            line = playerfile.readline()
+            
+            try:
+                if line[0] == '#': # comments
+                    continue
+            except IndexError:
+                pass
+
+            line[:-1] #Remove leading newline
+            cMain.playerlist.append(line.split())
+            
+        playerfile.close()
+
+        try:
+            cMain.playerlist.pop() #Remove last item, it is garbage
+        except IndexError:
+            pass
+        
+        for index in range(len(cMain.playerlist)):
+            cMain.playerlist[index][1] = int(cMain.playerlist[index][1])
+            cMain.playerlist[index][2] = int(cMain.playerlist[index][2])
+            cMain.playerlist[index][3] = int(cMain.playerlist[index][3])
+            cMain.playerlist[index][4] = int(cMain.playerlist[index][4])
+
+    def savePlayerFile(self):
+        try:
+            playerfile = open("players.save", "w")
+        except IOError:
+            print "Saving unsuccesful."
+            return
+
+        fprintf(playerfile, "# name games wins losses quits\n")
+        for player in cMain.playerlist:
+            fprintf(playerfile, "%s %i %i %i %i\n", player[0], player[1], player[2], player[3], player[4])
+        playerfile.close()
+
+    def newGame(self):
+        p1name = raw_input("Player 1, enter your name: ")
+        pid1 = cPlayerHandler.getPID(self, p1name)
+        if pid1 == -1:
+            print "No such player found. Creating.."
+            pid1 = cPlayerHandler.addPlayer(self, p1name)
+
+        p2name = raw_input("Player 2, enter your name: ")
+        pid2 = cPlayerHandler.getPID(self, p2name)
+        if pid2 == -1:
+            print "No such player found. Creating.."
+            pid2 = cPlayerHandler.addPlayer(self, p2name)
+
+        cGame.__init__(self, pid1, pid2) #Start new game
